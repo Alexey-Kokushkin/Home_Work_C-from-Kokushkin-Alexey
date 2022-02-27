6a8c3e8e248a44a62f4ef31b29fe3e82be04e9a4
Alexey-Kokushkin <kokushkin_83@mail.ru>
2022-02-27 14:31:15 +0900

    Task4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число N: ");
+String inputnumber = Console.ReadLine();
+int N = int.Parse(inputnumber);
+int A = 1;
+while (A <= N)
+{
+    int evennumbber = A % 2;
+    if (evennumbber == 0)
+    {
+        Console.Write(A + " ");
+    }
+    A = A + 1;
+}
+Console.WriteLine(" - Все четные числа в промежутке от 1 до введенного числа N");
